d291201d4378adb75a60581a1c1d925bd55983b1
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 17:23:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,11 +9,4 @@ def prime?(number)
 end 
 
 
-def prime?(number)
-  num = (2...number-1) && num != 0
-  if number/num == 1 && number/num != num
-  return true
-  elsif number == 0 || number > 0 || number == 1
-  return false
-end
-end
+
