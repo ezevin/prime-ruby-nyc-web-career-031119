5ae0fe28e79e1d6663882d3cf352686fd17d2b70
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-11 16:16:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
+
 # Add  code here!
+
+
+
+
+def prime?(number)
+  num = (2...number-1) && num != 0
+  if number/num == 1 && number/num != num
+  return true
+  elsif number == 0 || number > 0 || number == 1
+  return false
+end
+end
